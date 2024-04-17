16d47ac05d88310b9348b780728efd046cf209c3
wavewavewavewave <nikitashkrabov@gmail.com>
2024-04-17 13:07:06 +0300

    create about page

--- src/app/components/Footer/Footer.tsx
+++ src/app/components/Footer/Footer.tsx
@@ -8,38 +8,49 @@ import inst from '../../assets/Social_icons/instagram.svg'
 import tiktok from '../../assets/Social_icons/tik-tok.svg'
 import butt from '../../assets/call_me.svg'
 import {payments_icon} from "../../constants/Constants";
+import color_logo from '../../assets/color_logo.svg'
+import white_butt from '../../assets/Social_icons/кнопка.svg'
+import white_inst from '../../assets/Social_icons/кнопка instagram.svg'
+import white_tiktok from '../../assets/Social_icons/кнопка тик ток.svg'
 
-const Footer = () => {
+const Footer = ({clientPage}) => {
 
     return (
-        <div className={styles.container}>
-            <div className={styles.imageContainer}>
-                <Image src={line} alt={'Линия'}/>
-                <div className={styles.logo}>
-                    <Image src={logo} alt={'Лого'} />
-                </div>
-            </div>
-            <div className={styles.contactBlock}>
-                <div className={styles.contactInfo}>
-                    <div className={styles.textBlock}>
-                        <span className={styles.text}>4966866@GMAIL.COM</span>
-                        <span className={styles.text} style={{ whiteSpace: 'nowrap' }}>+375 (44) 496-68-66</span>
+        <div className={styles.container} style={clientPage === true ? {
+            background: 'linear-gradient(to bottom, #000000 0%, #666666 100%)',
+            marginTop: '0'
+        } : {}}>
+            <div style={{top: '300px'}}>
+                <div className={styles.imageContainer}>
+                    <Image src={line} alt={'Линия'}/>
+                    <div className={styles.logo} style={clientPage === true ? {left: '50%'} : {}}>
+                        <Image src={clientPage === true ? color_logo : logo} alt={'Лого'}/>
                     </div>
                 </div>
-                <Image src={inst} alt={'Инстаграмм'}/>
-                <Image src={tiktok} alt={'ТикТок'}/>
-                <button className={styles.button}>
-                    <Image src={butt} alt={'Связаться'}/>
-                </button>
-            </div>
-            <div className={styles.gallery}>
-                {payments_icon.map((item) => (
-                    <div className={styles.item} key={item.id}>
-                        <div className={styles.itemBox}>
-                            <Image key={item.id} src={item.value} alt={item.alt}/>
+                <div className={styles.contactBlock}>
+                    <div className={styles.contactInfo}>
+                        <div className={styles.textBlock}>
+                            <span className={styles.text}
+                                  style={clientPage ? {color: 'white'} : {}}>4966866@GMAIL.COM</span>
+                            <span className={styles.text}
+                                  style={clientPage ? {color: 'white', whiteSpace: 'nowrap'} : {whiteSpace: 'nowrap'}}>+375 (44) 496-68-66</span>
                         </div>
                     </div>
-                ))}
+                    <Image src={clientPage ? white_inst : inst} alt={'Инстаграмм'}/>
+                    <Image src={clientPage ? white_tiktok : tiktok} alt={'ТикТок'}/>
+                    <button className={styles.button}>
+                        <Image src={clientPage ? white_butt : butt} alt={'Связаться'}/>
+                    </button>
+                </div>
+                <div className={styles.gallery}>
+                    {payments_icon.map((item) => (
+                        <div className={styles.item} key={item.id}>
+                            <div className={styles.itemBox}>
+                                <Image key={item.id} src={item.value} alt={item.alt}/>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
--- src/app/pages/about/page.tsx
+++ src/app/pages/about/page.tsx
@@ -7,29 +7,36 @@ import poster from '../../assets/постер.jpg'
 import vector from '../../assets/Vector.svg'
 import Image from "next/image";
 import HighlightKeywords from "../../components/HighlightKeywords/HighlightKeywords";
+import Footer from "../../components/Footer/Footer";
 
 const About = () => {
     return (
-        <div className={styles.gradientBackground}>
-            <Header/>
-            <div className={styles.infoBlock}>
-                <div style={{width: '900px'}}>
-                    <Image src={poster} alt={'Постер'} width={900} height={1500} className={styles.posterStyle}/>
-                    <div className={{width: '500px'}}>
-                        <span className={`${styles.textStyle} ${styles.text1}`}>БОЛЕЕ 5000 м2 переработанно материалов</span>
-                        <span className={`${styles.textStyle} ${styles.text2}`}>БОЛЕЕ 80 км раскроя по периметру</span>
-                        <span className={`${styles.textStyle} ${styles.text3}`}>БОЛЕЕ 2000 вывесок изготовлено</span>
+        <>
+            <div className={styles.container}>
+                <Header/>
+                <div className={styles.infoBlock}>
+                    <div style={{width: '900px'}}>
+                        <Image src={poster} alt={'Постер'} width={900} height={1500} className={styles.posterStyle}/>
+                        <div className={{width: '500px'}}>
+                        <span className={`${styles.textStyle} ${styles.text1}`}>
+                            <span className={styles.textTest}>БОЛЕЕ 5000 м2</span> переработанно материалов</span>
+                            <span className={`${styles.textStyle} ${styles.text2}`}>
+                            <span className={styles.textTest}>БОЛЕЕ 80 км</span> раскроя по периметру</span>
+                            <span className={`${styles.textStyle} ${styles.text3}`}>
+                            <span className={styles.textTest}>БОЛЕЕ 2000</span> вывесок изготовлено</span>
+                        </div>
                     </div>
-                </div>
-                <div className={styles.textBlock}>
-                    <div className={styles.infoBlock}>
-                        <h1 className={styles.title}>О НАС</h1>
-                        <Image src={vector} alt={'Вектор'} className={styles.vectorStyle}/>
+                    <div className={styles.textBlock}>
+                        <div className={styles.infoBlock}>
+                            <h1 className={styles.title}>О НАС</h1>
+                            <Image src={vector} alt={'Вектор'} className={styles.vectorStyle}/>
+                        </div>
+                        <HighlightKeywords textArray={about_text_array}/>
                     </div>
-                    <HighlightKeywords textArray={about_text_array}/>
                 </div>
             </div>
-        </div>
+            <Footer clientPage={true}/>
+        </>
     );
 };
 
