c053a8e4b32384f81d8a2980df3538f28d58a0b1
wavewavewavewave <nikitashkrabov@gmail.com>
2024-04-19 11:59:37 +0300

    update nextConfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: '/Author_new/'
+    basePath: '/Author_new/',
+    reactStrictMode: true,
 };
 
 export default nextConfig;
