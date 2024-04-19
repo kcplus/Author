497128780516ebc963efac988e4f77668992d063
wavewavewavewave <nikitashkrabov@gmail.com>
2024-04-19 13:16:21 +0300

    update nextConfig

---
+++ dist/_next/static/chunks/app/pages/portfolio/page-de2db1b1a3f0e2c3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[3411],{6033:function(n,e,u){Promise.resolve().then(u.bind(u,1093))}},function(n){n.O(0,[8491,6648,1093,2971,7023,1744],function(){return n(n.s=6033)}),_N_E=n.O()}]);
