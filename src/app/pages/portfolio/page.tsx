edef86ddf8a483f9c4c5d1ccf618e469a31e999b
wavewavewavewave <nikitashkrabov@gmail.com>
2024-04-22 02:10:59 +0300

    update fonts, refactor code, fix bugs

--- src/app/pages/portfolio/page.tsx
+++ src/app/pages/portfolio/page.tsx
@@ -4,27 +4,8 @@ import Header from "../../components/Header/Header";
 import vector from '../../assets/Vector.svg'
 import Image from "next/image";
 import styles from './portfolio.module.css'
-import { ReactPhotoCollage } from "react-photo-collage";
-import image1 from '../../assets/коллаж из рекламы2.jpg'
-import image2 from '../../assets/Pages/Signs/image 50.jpg'
-import image3 from '../../assets/Pages/Signs/image 51.jpg'
-import image$ from '../../assets/Pages/Signs/image 53.jpg'
-import image5 from '../../assets/Pages/Signs/image 54.jpg'
 
 const Portfolio = () => {
-    /*const setting = {
-        width: '100vw',
-        height: ['250px', '160px'],
-        layout: [2, 2],
-        photos: [
-            { source:'../../assets/коллаж из рекламы2.jpg' },
-            { source: image2 },
-            { source: image3 },
-            { source: image$ },
-            { source: image5 },
-        ],
-    };*/
-
 
     return (
         <div>
@@ -33,7 +14,6 @@ const Portfolio = () => {
             <h1 className={styles.title}>Наши работы</h1>
             <Image src={vector} alt={'VECTOR'} style={{position: 'absolute', right: '580px', top: '172px'}}/>
             </div>
-            {/*<ReactPhotoCollage {...setting}/>*/}
         </div>
     );
 };
